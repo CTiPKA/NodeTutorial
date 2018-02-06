1a6226d4621249cdf491637f7261f6d3982cb059
Vadim Trulyaev <vtrulyaev@iba.by>
2018-02-06 14:46:26 +0300

    MongoDB driver added

--- index.js
+++ index.js
@@ -1,26 +1,70 @@
 var http = require("http"),
   express = require("express"),
-  path = require("path");
+  path = require("path"),
+  MongoClient = require("mongodb").MongoClient,
+  Server = require("mongodb").Server,
+  CollectionDriver = require("./collectionDriver").CollectionDriver;
 
 var app = express();
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(express.static(path.join(__dirname, "public")));
+var mongoHost = "localHost";
+var mongoPort = 27017;
+var collectionDriver;
 
-app.get("/", function(req, res) {
-  res.send("<html><body><h1>Hello World</h1></body></html>");
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort));
+mongoClient.connect(function(err, mongoClient) {
+  if (!mongoClient) {
+    console.error("Error! Exiting... Must start MongoDB first");
+    process.exit(1);
+  }
+  var db = mongoClient.db("MyDatabase");
+  collectionDriver = new CollectionDriver(db);
 });
 
-app.get("/:a?/:b?/:c?", function(req, res) {
-  res.send(req.params.a + " " + req.params.b + " " + req.params.c);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/:collection", function(req, res) {
+  var params = req.params;
+  collectionDriver.findAll(req.params.collection, function(error, objs) {
+    if (error) {
+      res.send(400, error);
+    } else {
+      if (req.accepts("html")) {
+        res.render("data", {
+          objects: objs,
+          collection: req.params.collection
+        });
+      } else {
+        res.set("Content-Type", "application/json");
+        res.send(200, objs);
+      }
+    }
+  });
+});
+
+app.get("/:collection/:entity", function(req, res) {
+  var params = req.params;
+  var entity = params.entity;
+  var collection = params.collection;
+  if (entity) {
+    collectionDriver.get(collection, entity, function(error, objs) {
+      if (error) {
+        res.send(400, error);
+      } else {
+        res.send(200, objs);
+      }
+    });
+  } else {
+    res.send(400, { error: "bad url", url: req.url });
+  }
 });
 
 //wrong address error handing
 app.use(function(req, res) {
-  //1
-  res.render("404", { url: req.url }); //2
+  res.render("404", { url: req.url });
 });
 
 http.createServer(app).listen(app.get("port"), function() {
